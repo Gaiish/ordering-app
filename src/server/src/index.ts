2b385f81119a3d080f2d9ac8d8e769d2d044f6b1
Gaiish <gdishim@gmail.com>
2021-05-06 18:10:12 +0300

    dockerize server and create server.yml CI

--- src/server/src/index.ts
+++ src/server/src/index.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (_, res) => {
-  res.status(200).json({ data: "test" });
+app.get('/', (_, res) => {
+  res.status(200).json({ data: 'test' });
 });
 
 app.listen(PORT, () => console.log(`> Ready on port ${PORT}`));
