f901157c67144aa3cb0d31c028cca39ea3b63b1e
Gaiish <gdishim@gmail.com>
2021-05-08 20:59:30 +0300

    update endpoints responses

--- src/server/src/routes/ordersRouter.ts
+++ src/server/src/routes/ordersRouter.ts
@@ -15,9 +15,15 @@ ordersRouter.use(verifyAuthorization);
 ordersRouter.get('/', async (_, res) => {
   try {
     const orders: IOrders = await getAllOrders();
-    res.status(200).json(orders);
+    res.status(200).json({
+      status: 200,
+      data: orders,
+    });
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).json({
+      status: 500,
+      message: error.message,
+    });
   }
 });
 
@@ -25,30 +31,40 @@ ordersRouter.get('/:id', async (req, res) => {
   try {
     const order: IOrder | undefined = await getOrderById(req.params.id);
     if (order) {
-      res.status(200).json(order);
+      res.status(200).json({
+        status: 200,
+        data: order,
+      });
     } else {
-      res.status(404).send('Order not found');
+      res.status(404).json({
+        status: 404,
+        message: 'Order not found',
+      });
     }
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).json({ status: 500, message: error.message });
   }
 });
 
 ordersRouter.post('/', async (req, res) => {
   try {
     const orderId = await createOrder(req.body);
-    res.status(201).json(orderId);
+    res.status(201).json({
+      status: 201,
+      message: 'New order created!',
+      data: { orderId },
+    });
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).json({ status: 500, message: error.message });
   }
 });
 
 ordersRouter.put('/:id', async (req, res) => {
   try {
     await updateOrder(req.params.id, req.body);
-    res.status(200).json({ message: 'success, order updated' });
+    res.status(200).json({ status: 200, message: 'Success, order updated' });
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).json({ status: 500, message: error.message });
   }
 });
 
