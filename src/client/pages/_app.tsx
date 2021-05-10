c863a8e24ff15e241ea502b3667d5b695edd58e0
Gaiish <gdishim@gmail.com>
2021-05-11 02:16:37 +0300

    client setup: add styled-components, firebase, vercel

--- src/client/pages/_app.tsx
+++ src/client/pages/_app.tsx
@@ -1,7 +1,30 @@
-import '../styles/globals.css';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+`;
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default MyApp;
