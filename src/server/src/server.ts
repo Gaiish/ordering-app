a6e3e0de9dd78284c66584316f1c2caa24c5d778
Gaiish <gdishim@gmail.com>
2021-05-10 17:03:25 +0300

    add logging middleware: winston

--- src/server/src/server.ts
+++ src/server/src/server.ts
@@ -1,17 +1,31 @@
 import express from 'express';
 import cors from 'cors';
+import * as winston from 'winston';
+import * as expressWinston from 'express-winston';
 
 import ordersRouter from './routes/ordersRouter';
 
 const server = express();
 
+const loggerOpts: expressWinston.LoggerOptions = {
+  transports: [new winston.transports.Console()],
+  format: winston.format.combine(
+    winston.format.json(),
+    winston.format.prettyPrint(),
+    winston.format.colorize({ all: true }),
+  ),
+};
+
 server.use(express.json());
 server.use(cors());
+server.use(expressWinston.logger(loggerOpts));
 
 server.use('/orders', ordersRouter);
 
 server.use((_, res) => {
   res.send('Ordering app backend: Nothing here!');
 });
+
+server.use(expressWinston.errorLogger(loggerOpts));
 
 export default server;
